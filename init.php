52edb382ab3d51e382112da60ded704725280252
Drarok Ithaqua <github@catchall.drarok.com>
2012-02-15 11:40:38 +0000

    Move pheanstalk up one level in the vendor directory.

--- init.php
+++ init.php
@@ -3,6 +3,6 @@
  * Pheanstalk compatibility with Kohana's autoload implementation.
  */
 
-if ($path = Kohana::find_file('vendor', 'pheanstalk/pheanstalk_init')) {
+if ($path = Kohana::find_file('vendor', 'pheanstalk_init')) {
 	require_once $path;
 }
