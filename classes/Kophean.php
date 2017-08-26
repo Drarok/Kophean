04f68eb6d298b5ca51868d9eeb9d740c3471bb22
Drarok Ithaqua <github@catchall.drarok.com>
2017-08-26 16:32:08 +0100

    Rename class file to latest standard, and use latest PHP coding standards

---
+++ classes/Kophean.php
@@ -0,0 +1,35 @@
+<?php
+
+abstract class Kophean
+{
+    /**
+     * Instantiate and configure a Pheanstalk instance.
+     *
+     * @param string $config Config section name
+     *
+     * @return Pheanstalk
+     */
+    public static function factory($config = 'default')
+    {
+        $config = Kohana::$config->load('kophean.' . $config);
+        $instance = new Pheanstalk(Arr::get($config, 'server'));
+
+        if ((bool) $use = Arr::get($config, 'use-tube')) {
+            $instance->useTube($use);
+        }
+
+        if ((bool) $watch = Arr::get($config, 'watch-tubes')) {
+            foreach ($watch as $tube) {
+                $instance->watch($tube);
+            }
+        }
+
+        if ((bool) $ignore = Arr::get($config, 'ignore-tubes')) {
+            foreach ($ignore as $tube) {
+                $instance->ignore($tube);
+            }
+        }
+
+        return $instance;
+    }
+}
